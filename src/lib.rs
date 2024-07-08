b7bde82ae1869c0090f8053ab91f58f8e8ba460c
Lukas Olsen <lukmarwil@gmail.com>
2024-07-08 21:47:27 +0200

    feat(cors fix): a simply quick cors trial

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use mock_data::{GenerateDataRequest, MockDataGenerator};
 use once_cell::sync::Lazy;
 
 pub type MockDataSchema =
-    Schema<QueryRoot, async_graphql::EmptyMutation, async_graphql::EmptySubscription>;
+    Schema<QueryRoot, MutationRoot, async_graphql::EmptySubscription>;
 
 #[derive(SimpleObject)]
 struct GenerateDataResponse {
@@ -16,9 +16,24 @@ struct GenerateDataResponse {
 }
 
 pub struct QueryRoot;
+pub struct MutationRoot;
 
 #[Object]
 impl QueryRoot {
+    async fn generate_data(
+        &self,
+        _ctx: &Context<'_>,
+        request: GenerateDataRequest,
+    ) -> GenerateDataResponse {
+        match GENERATOR.generate_data(&request) {
+            Ok(result) => GenerateDataResponse { result },
+            Err(err) => panic!("Error generating data: {}", err),
+        }
+    }
+}
+
+#[Object]
+impl MutationRoot {
     async fn generate_data(
         &self,
         _ctx: &Context<'_>,
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use actix_web::{web, App, HttpServer};
-use async_graphql::{EmptyMutation, Schema};
+use actix_web::{http, web, App, HttpServer};
+use actix_cors::Cors;
+use async_graphql::Schema;
 use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
-use mock_data::{MockDataSchema, QueryRoot};
+use mock_data::{MockDataSchema, QueryRoot, MutationRoot};
 
 async fn graphql_handler(
     schema: web::Data<MockDataSchema>,
@@ -13,10 +14,18 @@ async fn graphql_handler(
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Server running on 127.0.0.1:7070!");
-    let schema = Schema::build(QueryRoot, EmptyMutation, async_graphql::EmptySubscription).finish();
+    let schema = Schema::build(QueryRoot, MutationRoot, async_graphql::EmptySubscription).finish();
 
     HttpServer::new(move || {
+        let cors = Cors::default()
+              .allowed_origin("http://localhost:5173")
+              .allowed_methods(vec!["GET", "POST"])
+              .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+              .allowed_header(http::header::CONTENT_TYPE)
+              .max_age(3600);
+
         App::new()
+            .wrap(cors)
             .app_data(web::Data::new(schema.clone()))
             .route("/graphql", web::post().to(graphql_handler))
     })
